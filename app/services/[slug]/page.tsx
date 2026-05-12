471ce7e32a6d19b2beea9617124259c00e72550e
Alexey <mskdemin@gmail.com>
2026-05-12 13:03:46 +0300

    fix service images display

--- app/services/[slug]/page.tsx
+++ app/services/[slug]/page.tsx
@@ -209,10 +209,10 @@ export default async function ServicePage({
                 <SectionCardImage
                   image={service.image}
                   alt={service.title}
-                  width={1440}
-                  height={840}
+                  width={960}
+                  height={540}
                   fit="max"
-                  sizes="(max-width: 720px) calc(100vw - 72px), (max-width: 1180px) calc(100vw - 88px), 1092px"
+                  sizes="(max-width: 720px) calc(100vw - 100px), (max-width: 1180px) 68vw, 680px"
                   wrapperClassName="articlePageMedia servicePageMedia"
                   imageClassName="articlePageImage servicePageImage"
                   ignoreCrop
